3739fd33ab0bbfc2ddc681e1eea2bc3ecdc030bb
HMSagar2701 <sagarhm2701@gmail.com>
2025-03-28 17:37:54 +0530

    Added nav bar

---
+++ frontend/app/components/landingPage/Banner.tsx
@@ -0,0 +1,11 @@
+export const Banner = () => {
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,_#FCD6FF,_#29D8FF,_#FFFD80,_#F89ABF,#FCD6FF)]">
+      <div className="container">
+        <div className="hidden sm:inline font-medium text-black">
+          On-Chain Wallet Scoring is now live! 🚀
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ frontend/app/components/landingPage/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className='container h-full mt-10 mx-auto'>
+        <div>
+            OCWR
+        </div>
+    </div>
+  )
+}
