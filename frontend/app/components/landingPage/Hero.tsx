224333e2112fb74c29c09cdad6d6de773c9edc88
UdayPandey01 <pandeyuday113@gmail.com>
2025-03-30 16:04:42 +0530

    added wallet function

--- frontend/app/components/landingPage/Hero.tsx
+++ frontend/app/components/landingPage/Hero.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+// import { Profile } from './Profile'
+// import { Accounts } from './Accounts'
+// import { ConnectButton } from '@rainbow-me/rainbowkit'
 export const Hero = () => {
   return (
     <div className='container h-full mt-10 mx-auto'>
         <div>
-            OCWR
+            {/* <Accounts />
+            <Profile /> */}
+          {/* <ConnectButton /> */}
         </div>
     </div>
   )
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,4 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Banner } from "./components/landingPage/Banner";
 import { Hero } from "./components/landingPage/Hero";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <Banner />
     <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
     <Hero />
+    <ConnectButton/>
     </div>
   );
 }
